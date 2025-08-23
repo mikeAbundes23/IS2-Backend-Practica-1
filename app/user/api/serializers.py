f6cf7a2a417b92d322afb1f91928044eb5e19942
AlbertoBarradas <sebastianbago@hotmail.com>
2025-08-22 23:54:15 -0600

    Se creo el modelo Task y se implemento la API CRUD

--- app/user/api/serializers.py
+++ app/user/api/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework.serializers import ModelSerializer
-from user.models import User
+from user.models import User, Task
 
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+
+class TaskSerializer(ModelSerializer):
+    class Meta:
+        model = Task
+        fields = ['id', 'title', 'description', 'status', 'owner']
--- app/user/api/views.py
+++ app/user/api/views.py
@@ -3,9 +3,9 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import get_object_or_404
-
-from user.models import User
-from .serializers import UserSerializer
+from rest_framework import generics, permissions
+from user.models import User, Task
+from .serializers import UserSerializer, TaskSerializer
 
 
 @api_view(['GET'])
@@ -14,3 +14,17 @@ def users(request):
     users = User.objects.all()
     serializer = UserSerializer(users, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class TaskListCreateView(generics.ListCreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class TaskDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]

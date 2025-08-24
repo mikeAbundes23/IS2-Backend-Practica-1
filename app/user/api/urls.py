710a85c177f5fb2a6eb82ec2604ab58a79616e02
mikeAbundes23 <irvingmiguel.abundes@ciencias.unam.mx>
2025-08-23 19:44:11 -0600

    feat: integración de autenticación JWT y mejoras en la API de usuario

--- app/user/api/urls.py
+++ app/user/api/urls.py
@@ -1,11 +1,29 @@
 from django.urls import path
 
 from .views import (
-    users, TaskDetailView, TaskListCreateView
-) 
+    users, TaskDetailView, TaskListCreateView,
+    get_routes, MyTokenObtainPairView, create_user, me, get_my_tasks
+)
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
+    path('', get_routes, name='get_routes'),
+
+    # Users
     path('users/', users, name='users'),
+    path('user/me/', me, name='me'),
+    path('user/create/', create_user, name='create_user'),
+    path('user/tasks/my-tasks/', get_my_tasks, name='my-tasks'),
+
+    # Tasks
     path('tasks/', TaskListCreateView.as_view(), name='task-list-create'),
     path('tasks/<int:pk>/', TaskDetailView.as_view(), name='task-detail'),
+
+    # JWT
+    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- app/user/api/views.py
+++ app/user/api/views.py
@@ -7,6 +7,58 @@ from rest_framework import generics, permissions
 from user.models import User, Task
 from .serializers import UserSerializer, TaskSerializer
 
+# JWT: Importa las vistas necesarias para el manejo de tokens
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+@api_view(['GET'])
+def get_routes(request):
+    routes = [
+        'api/token',
+        'api/token/refresh',
+    ]
+
+    return Response(routes)
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['id'] = user.id
+
+        return token
+    
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+    
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def create_user(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        user = serializer.save()
+        return Response(UserSerializer(user).data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def me(request):
+    user = get_object_or_404(User, pk=request.user.id)
+    serializer = UserSerializer(user)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_my_tasks(request):
+    tasks = Task.objects.filter(owner=request.user)
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
 
 @api_view(['GET'])
 @permission_classes([AllowAny])
